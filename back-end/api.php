5a26cc2c64e69626b380f8bfd3db7142c092a5aa
jrrj67 <ricardojunior.6767@gmail.com>
2020-03-23 13:40:22 -0300

    Adicionando button backspace

--- back-end/api.php
+++ back-end/api.php
@@ -1,1 +1,24 @@
 <?php
+
+$input = $_POST['input'];
+$values = explode(" ", $input);
+$strValues = str_replace('x', '*', implode($values));
+validate($strValues);
+
+function validate($strValues)
+{
+    if (!strpbrk($strValues, '1234567890+-*/')) {
+        echo 'Valor inválido';
+    } else {
+        try {
+            $result = @eval("return $strValues;");
+            if($result === INF) {
+                echo 'Divisão por zero é impossível';
+            } else {
+                echo $result;
+            }
+        } catch (ParseError $error) {
+            echo 'Valor inválido';
+        }
+    }
+}
--- index.php
+++ index.php
@@ -20,44 +20,60 @@
                 <div class="card">
                     <div class="card-content">
                         <div class="row">
-                            <div class="col s4 center">
-                                <a class="waves-effect waves-light btn" onclick="typeNumber(7)">7</a>
+                            <div class="">
+                                <a class="col l2 push-l2 waves-effect waves-light btn" onclick="typeNumber(7)">7</a>
                             </div>
-                            <div class="col s4 center">
-                                <a class="waves-effect waves-light btn" onclick="typeNumber(8)">8</a>
+                            <div class="">
+                                <a class="col l2 push-l3 waves-effect waves-light btn" onclick="typeNumber(8)">8</a>
                             </div>
-                            <div class="col s4 center">
-                                <a class="waves-effect waves-light btn" onclick="typeNumber(9)">9</a>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col s4 center">
-                                    <a class="waves-effect waves-light btn" onclick="typeNumber(4)">4</a>
-                            </div>
-                            <div class="col s4 center">
-                                <a class="waves-effect waves-light btn" onclick="typeNumber(5)">5</a>
-                            </div>
-                            <div class="col s4 center">
-                                <a class="waves-effect waves-light btn" onclick="typeNumber(6)">6</a>
+                            <div class="">
+                                <a class="col l2 push-l4 waves-effect waves-light btn" onclick="typeNumber(9)">9</a>
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col s4 center">
-                                <a class="waves-effect waves-light btn" onclick="typeNumber(3)">3</a>
+                            <div class="">
+                                <a class="col l2 push-l2 waves-effect waves-light btn" onclick="typeNumber(4)">4</a>
                             </div>
-                            <div class="col s4 center">
-                                <a class="waves-effect waves-light btn" onclick="typeNumber(2)">2</a>
+                            <div class="">
+                                <a class="col l2 push-l3 waves-effect waves-light btn" onclick="typeNumber(5)">5</a>
                             </div>
-                            <div class="col s4 center">
-                                <a class="waves-effect waves-light btn" onclick="typeNumber(1)">1</a>
+                            <div class="">
+                                <a class="col l2 push-l4 waves-effect waves-light btn" onclick="typeNumber(6)">6</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="">
+                                <a class="col l2 push-l8 waves-effect waves-light btn" onclick="typeNumber(3)">3</a>
+                            </div>
+                            <div class="">
+                                <a class="col l2 push-l3 waves-effect waves-light btn" onclick="typeNumber(2)">2</a>
+                            </div>
+                            <div class="">
+                                <a class="col l2 pull-l2 waves-effect waves-light btn" onclick="typeNumber(1)">1</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="">
+                                <a class="col l5 push-l2 waves-effect waves-light btn" onclick="typeNumber(0)">0</a>
+                            </div>
+                            <div class="center">
+                                <a class="col l2 push-l3 waves-effect waves-light btn" onclick="clearBtn()">C</a>
                             </div>
                         </div>
                         <div class="row">
                             <div class="divider"></div>
                         </div>
-                        <h6 id="input" class="">
-
-                        </h6>
+                        <div class="row">
+                            <div class="input-field col l12">
+                                <a class="col l3 push-l10 z-depth-0 transparent btn">
+                                    <i class="tiny material-icons" style="color:grey;">backspace</i>
+                                </a>
+                                <form action="#" onsubmit="resultBtn()">
+                                    <input id="input" onsubmit="resultBtn()" type="text" value="" autocomplete="off">
+                                    <label for="input"></label>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -69,25 +85,22 @@
                                 <a class="col l5 waves-effect waves-light btn-small" onclick="typeOperator(' - ')">-</a>
                             </div>
                             <div class="center">
-                                <a class="col l5 push-l2 waves-effect waves-light btn-small" onclick="typeOperator(' + ')">+</a>
+                                <a class="col l5 push-l2 waves-effect waves-light btn-small"
+                                   onclick="typeOperator(' + ')">+</a>
                             </div>
                         </div>
                         <div class="row">
                             <div class="center">
-                                <a class="col l5 waves-effect waves-light btn-small" onclick="typeOperator(' * ')">*</a>
+                                <a class="col l5 waves-effect waves-light btn-small" onclick="typeOperator(' x ')">x</a>
                             </div>
                             <div class="center">
-                                <a class="col l5 push-l2 waves-effect waves-light btn-small" onclick="typeOperator(' / ')">/</a>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="center">
-                                <a class="col l5 waves-effect waves-light btn-small" onclick="clearBtn()">C</a>
+                                <a class="col l5 push-l2 waves-effect waves-light btn-small"
+                                   onclick="typeOperator(' / ')">/</a>
                             </div>
                         </div>
                         <div class="row">
                             <div class="center">
-                                <a class="col l12 waves-effect waves-light btn-small">=</a>
+                                <a class="col l12 waves-effect waves-light btn-small" onclick="resultBtn()">=</a>
                             </div>
                         </div>
                     </div>
@@ -101,17 +114,39 @@
 </footer>
 <script src="js/materialize.js"></script>
 <script>
+
     function typeNumber(number) {
-        document.getElementById('input').innerHTML += number;
+        document.getElementById('input').value += number;
     }
 
     function typeOperator(operator) {
-        document.getElementById('input').innerHTML += operator;
+        document.getElementById('input').value += operator;
     }
 
     function clearBtn() {
-        document.getElementById('input').innerHTML = '';
+        document.getElementById('input').value = '';
     }
+
+    function resultBtn() {
+
+        const data = new FormData();
+
+        data.append('input', document.getElementById('input').value);
+
+        const ajax = new XMLHttpRequest();
+
+        ajax.open("POST", "back-end/api.php", true);
+
+        ajax.send(data);
+
+        ajax.onreadystatechange = function () {
+            if (ajax.readyState == 4 && ajax.status == 200) {
+                var response = ajax.responseText;
+                document.getElementById('input').value = response;
+            }
+        }
+    }
+
 
 </script>
 </body>
