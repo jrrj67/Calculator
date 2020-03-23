dabb1fca953d5939712ff72e4ef47d92aebf30cf
jrrj67 <ricardojunior.6767@gmail.com>
2020-03-23 14:48:18 -0300

    Adicionando sidebar

--- index.php
+++ index.php
@@ -11,53 +11,64 @@
 </head>
 <body>
 <header>
+    <nav class="hide-on-med-and-up">
+        <div class="nav-wrapper">
+            <a href="#" data-target="slide-out" class="sidenav-trigger hide-on-med-and-up"><i class="material-icons">menu</i></a>
+            <a href="#" class="brand-logo">Calculadora</a>
+        </div>
+    </nav>
+    <ul id="slide-out" class="sidenav sidenav-fixed">
+        <li><a href="#!">First Sidebar Link</a></li>
+        <li><a href="#!">Second Sidebar Link</a></li>
+    </ul>
 </header>
 <main>
     <div class="row"></div>
+    <div class="row"></div>
     <div class="container">
         <div class="row">
-            <div class="col s10 push-s1 l4 push-l3">
+            <div class="col s8 l4 push-l3">
                 <div class="card">
                     <div class="card-content">
                         <div class="row">
                             <div class="">
-                                <a class="col l2 push-l2 waves-effect waves-light btn" onclick="typeNumber(7)">7</a>
+                                <a class="col l2 push-l2 s2 push-s2 waves-effect waves-light btn" onclick="typeNumber(7)">7</a>
                             </div>
                             <div class="">
-                                <a class="col l2 push-l3 waves-effect waves-light btn" onclick="typeNumber(8)">8</a>
+                                <a class="col l2 push-l3 s2 push-s3 waves-effect waves-light btn" onclick="typeNumber(8)">8</a>
                             </div>
                             <div class="">
-                                <a class="col l2 push-l4 waves-effect waves-light btn" onclick="typeNumber(9)">9</a>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="">
-                                <a class="col l2 push-l2 waves-effect waves-light btn" onclick="typeNumber(4)">4</a>
-                            </div>
-                            <div class="">
-                                <a class="col l2 push-l3 waves-effect waves-light btn" onclick="typeNumber(5)">5</a>
-                            </div>
-                            <div class="">
-                                <a class="col l2 push-l4 waves-effect waves-light btn" onclick="typeNumber(6)">6</a>
+                                <a class="col l2 push-l4 s2 push-s4 waves-effect waves-light btn" onclick="typeNumber(9)">9</a>
                             </div>
                         </div>
                         <div class="row">
                             <div class="">
-                                <a class="col l2 push-l8 waves-effect waves-light btn" onclick="typeNumber(3)">3</a>
+                                <a class="col l2 push-l2 s2 push-s2 waves-effect waves-light btn" onclick="typeNumber(4)">4</a>
                             </div>
                             <div class="">
-                                <a class="col l2 push-l3 waves-effect waves-light btn" onclick="typeNumber(2)">2</a>
+                                <a class="col l2 push-l3 s2 push-s3 waves-effect waves-light btn" onclick="typeNumber(5)">5</a>
                             </div>
                             <div class="">
-                                <a class="col l2 pull-l2 waves-effect waves-light btn" onclick="typeNumber(1)">1</a>
+                                <a class="col l2 push-l4 s2 push-s4 waves-effect waves-light btn" onclick="typeNumber(6)">6</a>
                             </div>
                         </div>
                         <div class="row">
                             <div class="">
-                                <a class="col l5 push-l2 waves-effect waves-light btn" onclick="typeNumber(0)">0</a>
+                                <a class="col l2 push-l8 s2 push-s2 waves-effect waves-light btn" onclick="typeNumber(3)">3</a>
+                            </div>
+                            <div class="">
+                                <a class="col l2 push-l3 s2 push-s3 waves-effect waves-light btn" onclick="typeNumber(2)">2</a>
+                            </div>
+                            <div class="">
+                                <a class="col l2 pull-l2 s2 push-s4 waves-effect waves-light btn" onclick="typeNumber(1)">1</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="">
+                                <a class="col l5 push-l2 s5 push-s2 waves-effect waves-light btn" onclick="typeNumber(0)">0</a>
                             </div>
                             <div class="center">
-                                <a class="col l2 push-l3 waves-effect waves-light btn" onclick="clearBtn()">C</a>
+                                <a class="col l2 push-l3 s2 push-s3 waves-effect waves-light btn" onclick="clearBtn()">C</a>
                             </div>
                         </div>
                         <div class="row">
@@ -65,7 +76,7 @@
                         </div>
                         <div class="row">
                             <div class="input-field col l12">
-                                <a class="col l3 push-l10 z-depth-0 transparent btn">
+                                <a class="col l3 push-l10 s2 push-s10 z-depth-0 transparent btn" onclick="backSpace()">
                                     <i class="tiny material-icons" style="color:grey;">backspace</i>
                                 </a>
                                 <form action="#" onsubmit="resultBtn()">
@@ -77,30 +88,30 @@
                     </div>
                 </div>
             </div>
-            <div class="col l2 push-l3">
+            <div class="col l2 push-l3 s4">
                 <div class="card">
                     <div class="card-content">
                         <div class="row">
-                            <div class="center">
-                                <a class="col l5 waves-effect waves-light btn-small" onclick="typeOperator(' - ')">-</a>
+                            <div class="">
+                                <a class="col l5 s6 pull-s1 waves-effect waves-light btn-small" onclick="typeOperator(' - ')">-</a>
                             </div>
-                            <div class="center">
-                                <a class="col l5 push-l2 waves-effect waves-light btn-small"
+                            <div class="">
+                                <a class="col l5 push-l2 s6 push-s1 waves-effect waves-light btn-small"
                                    onclick="typeOperator(' + ')">+</a>
                             </div>
                         </div>
                         <div class="row">
-                            <div class="center">
-                                <a class="col l5 waves-effect waves-light btn-small" onclick="typeOperator(' x ')">x</a>
+                            <div class="">
+                                <a class="col l5 s6 pull-s1 waves-effect waves-light btn-small" onclick="typeOperator(' x ')">x</a>
                             </div>
-                            <div class="center">
-                                <a class="col l5 push-l2 waves-effect waves-light btn-small"
+                            <div class="">
+                                <a class="col l5 push-l2 s6 push-s1 waves-effect waves-light btn-small"
                                    onclick="typeOperator(' / ')">/</a>
                             </div>
                         </div>
                         <div class="row">
-                            <div class="center">
-                                <a class="col l12 waves-effect waves-light btn-small" onclick="resultBtn()">=</a>
+                            <div class="">
+                                <a class="col l12 s10 push-s1 waves-effect waves-light btn-small" onclick="resultBtn()">=</a>
                             </div>
                         </div>
                     </div>
@@ -125,6 +136,11 @@
 
     function clearBtn() {
         document.getElementById('input').value = '';
+    }
+
+    function backSpace() {
+        var myTextarea = document.getElementById('input');
+        myTextarea.value = myTextarea.value.substring(0, myTextarea.value.length - 1);
     }
 
     function resultBtn() {
